0a5cc1d2393122ab594300839cde3a90c360764c
m3vance <m3vance@gmail.com>
2023-06-08 13:53:06 -0700

    minor edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ function Header(args) {
     <Container>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -46,7 +46,6 @@ function Header(args) {
     </div>
   </div>
 </nav>
-    
     </Container>
   );
 }
