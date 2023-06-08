c3eb75872b345e637e8d1afda221804873594c48
cherrythefool <sarahandecherry@gmail.com>
2023-06-08 09:57:24 -0700

    first frontend commit, basic code only no styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import VillainEdit from "./pages/VillainEdit"
+import VillainIndex from "./pages/VillainIndex"
+import VillainNew from "./pages/VillainNew"
+import VillainShow from "./pages/VillainShow"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
 import './App.css';
+import { Routes, Route } from "react-router-dom"
 
-function App() {
+import mockVillains from "./mockVillains"
+
+const App = () => {
+  const [villains, setVillains] = useState(mockVillains)
+
+  console.log(villains)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/villainindex" element={<VillainIndex />} />
+        <Route path="/villainshow" element={<VillainShow />} />
+        <Route path="/villainnew" element={<VillainNew />} />
+        <Route path="/villainedit" element={<VillainEdit />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 
 export default App;
---
+++ src/pages/VillainIndex.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const VillainIndex = () => {
+    return (
+      <>
+      </>
+    )
+  }
+  
+export default VillainIndex;
