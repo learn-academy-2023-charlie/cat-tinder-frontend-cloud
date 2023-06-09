e6fc5769f0f2531c63df1bb378d13cecd2496162
m3vance <m3vance@gmail.com>
2023-06-09 16:16:14 -0700

    fixed index testing for read functionality

--- src/__tests__/VillainIndex.test.js
+++ src/__tests__/VillainIndex.test.js
@@ -1,18 +1,30 @@
-import { render, screen } from "@testing-library/react"
-import VillainIndex from "../pages/VillainIndex"
+import { render, screen } from "@testing-library/react";
+import VillainIndex from "../pages/VillainIndex";
 import mockVillains from "../mockVillains";
+import { BrowserRouter } from "react-router-dom";
 
 describe("<VillainIndex />", () => {
-    it("renders without crashing", () => {})
-    it("renders villain cards", () => {
-        const div = document.createElement("div")
-        render(<VillainIndex mockVillains={mockVillains} />, div)
-        mockVillains.forEach(villain => {
-            const villainName = screen.getByText(villain.name)
-            expect(villainName).toBeInTheDocument()
-        })
+  it("renders without crashing", () => {});
+  render(
+    <BrowserRouter>
+      <VillainIndex villains={mockVillains} />
+    </BrowserRouter>,
+    // div
+  );
+  it("renders villain cards", () => {
+    const div = document.createElement("div");
+    render(
+      <BrowserRouter>
+        <VillainIndex villains={mockVillains} />
+      </BrowserRouter>,
+      div
+    );
+    mockVillains.forEach(villain => {
+        screen.getByAltText(`profile of the villain ${villain.name}`
+        )
     })
-})
+    screen.logTestingPlaygroundURL();
+  });
+});
 
 
-//NOT currently passing - does not get name from page (line 11)
